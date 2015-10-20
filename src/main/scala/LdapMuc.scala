b24b956395a6925f12a800147ff8e0e1af0faba3
Andi Miller <andi@andimiller.net>
2015-10-20 02:33:41 +0100

    Add comments for LdapMuc

--- src/main/scala/LdapMuc.scala
+++ src/main/scala/LdapMuc.scala
@@ -14,6 +14,10 @@ class LdapMuc(conf: AuthConfig, subdomain: String, description: String, isHidden
 
   val ldaptools = new LDAPTools(conf)
 
+  /**
+   * Utility function to fail this IQ request
+   * @param iq
+   */
   def fail(iq: IQ): Unit = {
       val reply = IQ.createResultIQ(iq)
       reply.setChildElement(iq.getChildElement.createCopy())
@@ -21,25 +25,31 @@ class LdapMuc(conf: AuthConfig, subdomain: String, description: String, isHidden
       this.router.route(reply)
   }
 
+  /**
+   * Overriden copy of processPacket so that we can screen for registration requests and check them against LDAP,
+   * denying them if we believe the user shouldn't be allowed in.
+   * @param packet
+   */
   override def processPacket(packet: Packet): Unit = {
     packet match {
+      /*
+        We only want to intercept packets which are valid register requests, so we can check them against LDAP and screen them
+      */
       case iq: IQ if iq.getType!=IQ.Type.error &&
                      iq.getChildElement != null &&
                      iq.getChildElement.getNamespaceURI == "jabber:iq:register" &&
-                     Option(iq.getTo.getNode).map(this.getChatRoom).isDefined
-
+                     Option(iq.getTo.getNode).map(this.getChatRoom).isDefined // maybe remove this? we may want to intercept even if the room doesn't exist yet
                       =>
-
-        val target = iq.getTo.getNode
+        val target = iq.getTo.getNode // should be the room name
         ldaptools.loadUser(iq.getFrom.getNode) match {
           case Some(user) =>
             user.authGroup.contains(target) match {
-              case true => super.processPacket(packet)
-              case false => fail(iq)
+              case true => super.processPacket(packet) // if we think they should be allowed in, let the normal server take it from here
+              case false => fail(iq) // if they aren't in the group in LDAP, fail the request
             }
-          case None => fail(iq)
+          case None => fail(iq) // if the user doesn't exist in LDAP, fail the request
         }
-      case _ => super.processPacket(packet)
+      case _ => super.processPacket(packet) // fall through everything else to the normal server
     }
   }
 
